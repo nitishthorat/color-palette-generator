005024e0a91e349d28372ec28b99836dd83f0535
nitishthorat <nitishthorat112@gmail.com>
2024-08-29 16:40:03 -0700

    Added color picker for the input form adn color palette

--- src/components/ColorPalette/ColorPalette.js
+++ src/components/ColorPalette/ColorPalette.js
@@ -1,11 +1,21 @@
 import React, { useContext } from "react";
-import { Container, Table } from "react-bootstrap";
+import { Container, Table, Form } from "react-bootstrap";
 import { PaletteContext } from "../../providers/PaletteProvider";
+import "./ColorPalette.scss";
 
 const ColorPalette = () => {
-  const { palette } = useContext(PaletteContext);
+  const { palette, setPalette } = useContext(PaletteContext);
 
   const tableKeys = ["brand", "success", "error", "warning", "neutral"];
+
+  const handleColorChange = (category, index, newColor) => {
+    setPalette((prevPalette) => ({
+      ...prevPalette,
+      [category]: prevPalette[category].map((color, i) =>
+        i === index ? newColor : color
+      ),
+    }));
+  };
   return (
     <Container>
       <h3>Color Palette</h3>
@@ -29,7 +39,16 @@ const ColorPalette = () => {
                     key={index}
                     style={{ background: palette[key][rowIndex] }}
                   >
-                    {palette[key][rowIndex]}
+                    <div className="palette-cell">
+                      {palette[key][rowIndex]}
+                      <Form.Control
+                        type="color"
+                        value={palette[key][rowIndex]}
+                        onChange={(e) =>
+                          handleColorChange(key, rowIndex, e.target.value)
+                        }
+                      />
+                    </div>
                   </td>
                 ))}
               </tr>
--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,21 +1,33 @@
 import React, { useContext, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, InputGroup } from "react-bootstrap";
 import { PaletteContext } from "../../providers/PaletteProvider";
 
 const InputForm = () => {
   const { getPalette } = useContext(PaletteContext);
   const [brandColor, setBrandColor] = useState("");
+
   return (
     <Container>
       <h3>Preferences</h3>
       <Form>
         <Form.Group className="mb-3" controlId="brand-color">
           <Form.Label>Brand Color</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="#000000"
-            onChange={(e) => setBrandColor(e.target.value)}
-          />
+          <InputGroup className="mb-3">
+            <Form.Control
+              type="text"
+              placeholder="#000000"
+              value={brandColor}
+              onChange={(e) => setBrandColor(e.target.value)}
+            />
+            <Form.Control
+              type="color"
+              id="exampleColorInput"
+              value={brandColor}
+              onChange={(e) => {
+                setBrandColor(e.target.value);
+              }}
+            />
+          </InputGroup>
         </Form.Group>
       </Form>
       <Button onClick={() => getPalette(brandColor)}>Get Color Palette</Button>
